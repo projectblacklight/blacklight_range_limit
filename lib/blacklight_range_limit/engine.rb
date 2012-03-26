e1bb52575b7b9e6dbc220cb577473dbf8aa716b1
Jonathan Rochkind <jonathan@dnil.net>
2012-03-26 12:55:47 -0400

    automatically add excanvas.js to app precompile path
    More work needs to be done, it still won't be USED ever yet.

--- lib/blacklight_range_limit/engine.rb
+++ lib/blacklight_range_limit/engine.rb
@@ -5,6 +5,12 @@ require 'rails'
 module BlacklightRangeLimit
   class Engine < Rails::Engine
   
+    # Need to tell asset pipeline to precompile the excanvas
+    # we use for IE. 
+    initializer "blacklight_range_limit.assets", :after => "assets" do
+      Rails.application.config.assets.precompile += %w( flot/excanvas.min.js )
+    end
+    
     # Do these things in a to_prepare block, to try and make them work
     # in development mode with class-reloading. The trick is we can't
     # be sure if the controllers we're modifying are being reloaded in

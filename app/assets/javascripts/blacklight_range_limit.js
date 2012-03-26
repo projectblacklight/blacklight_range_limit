cafe50b68f76ffb4688c1587c0d70a331600489d
Jonathan Rochkind <jonathan@dnil.net>
2012-03-26 12:38:49 -0400

    plugin does NOT require jquery-ui

--- app/assets/javascripts/blacklight_range_limit.js
+++ app/assets/javascripts/blacklight_range_limit.js
@@ -3,7 +3,6 @@
 // require does not need to change if we change file list.
 
 //= require 'jquery'
-//= require 'jquery-ui'
 //= require 'flot/jquery.flot.js'
 //= require 'flot/jquery.flot.selection.js'
 //= require_tree './blacklight_range_limit'

9b51d12d2ef0953683bcc32fb5061f26319fba61
Jonathan Rochkind <jonathan@dnil.net>
2014-01-23 15:01:16 -0500

    fix routing for BL5

--- lib/blacklight_range_limit/route_sets.rb
+++ lib/blacklight_range_limit/route_sets.rb
@@ -1,12 +1,29 @@
 module BlacklightRangeLimit
+  # This module is monkey-patch included into Blacklight::Routes, so that
+  # map_resource will route to catalog#range_limit, for our action
+  # that fetches and returns range segments -- that action is
+  # also monkey patched into (eg) CatalogController.
   module RouteSets
-    protected
-    def catalog
-      add_routes do |options|
-        get 'catalog/range_limit' => 'catalog#range_limit'
-      end
+    extend ActiveSupport::Concern
 
-      super
+
+    included do |klass|
+      # Have to add ours BEFORE existing,
+      # so catalog/range_limit can take priority over
+      # being considered a document ID.
+      klass.default_route_sets = [:range_limit] + klass.default_route_sets
+    end
+
+
+    protected
+
+
+    # Add route for (eg) catalog/range_limit, pointing to the range_limit
+    # method we monkey patch into (eg) CatalogController.
+    def range_limit(primary_resource)
+      add_routes do |options|
+        get "#{primary_resource}/range_limit" => "#{primary_resource}#range_limit"
+      end
     end
   end
 end

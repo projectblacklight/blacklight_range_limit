7e102d8bc336009265f357a51e5e81a6abbf725a
Justin Coyne <justin@curationexperts.com>
2014-03-18 10:34:57 -0500

    Remove public/index.html when testing

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -3,6 +3,11 @@ require 'rails/generators'
 class TestAppGenerator < Rails::Generators::Base
   source_root File.expand_path("../../../../test_app_templates", __FILE__)
 
+  # This is only necessary for Rails 3
+  def remove_index
+    remove_file "public/index.html"
+  end
+
   def run_blacklight_generator
     say_status("warning", "GENERATING BL", :yellow)       
 

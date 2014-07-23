9d555c55f960d7f890df693889672898a0f102ad
Christopher Jesudurai <jchris@stanford.edu>
2014-07-23 16:00:05 -0700

    Adds accessible labels to range limit text fields

---
+++ spec/helpers/blacklight_range_limit_helper_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe "Blacklight Range Limit Helper" do
+
+  it "should render range text fields with/without labels" do 
+    expect(helper.render_range_input('pub_date', 'begin')).to match /^<input class=\"form-control range_begin\" id=\"range_pub_date_begin\" maxlength=\"4\"/
+    expect(helper.render_range_input('pub_date', 'begin', 'from pub date')).to match /^<label class=\"sr-only\" for=\"range_pub_date_begin\">from pub date<\/label>/
+  end
+
+end

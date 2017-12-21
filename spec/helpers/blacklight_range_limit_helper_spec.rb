40fa8e2aba9e5806bf2caff7964606dfb7ccd26f
Esmé Cowles <escowles@ticklefish.org>
2017-12-21 10:26:37 -0500

    Excluding page when building requests

--- spec/helpers/blacklight_range_limit_helper_spec.rb
+++ spec/helpers/blacklight_range_limit_helper_spec.rb
@@ -16,4 +16,22 @@ describe "Blacklight Range Limit Helper" do
     expect(html_maxlength_6).to have_css 'input.form-control.range_begin#range_pub_date_begin[maxlength="6"]'
   end
 
+  context "when building requests" do
+    let(:config) { Blacklight::Configuration.new }
+    before do
+      allow(helper).to receive(:blacklight_config).and_return(config)
+    end
+
+    it "should exclude page when adding a range" do
+      params = { q: '', page: '2' }
+      updated_params = helper.add_range('test', '1900', '1995', params)
+      expect(updated_params).not_to include(:page)
+    end
+
+    it "should exclude page when adding a missing range" do
+      params = { q: '', page: '2' }
+      updated_params = helper.add_range_missing('test', params)
+      expect(updated_params).not_to include(:page)
+    end
+  end
 end

039148889054c1c0a99a0a252fe93dedebd0cda8
Chris Beer <cabeer@stanford.edu>
2019-11-26 13:41:58 -0800

    Blacklight 7 moved #has_search_parameters? to the controller

--- lib/blacklight_range_limit/controller_override.rb
+++ lib/blacklight_range_limit/controller_override.rb
@@ -9,6 +9,7 @@ module BlacklightRangeLimit
     included do
       helper BlacklightRangeLimit::ViewHelperOverride
       helper RangeLimitHelper
+      helper_method :has_range_limit_parameters?
     end
 
     # Action method of our own!
@@ -26,6 +27,19 @@ module BlacklightRangeLimit
         search_builder.except(:add_range_limit_params).append(:fetch_specific_range_limit)
       end
       render('blacklight_range_limit/range_segments', :locals => {:solr_field => params[:range_field]}, :layout => !request.xhr?)
+    end
+
+    # over-ride, call super, but make sure our range limits count too
+    def has_search_parameters?
+      super || has_range_limit_parameters?
+    end
+
+    def has_range_limit_parameters?(my_params = params)
+      my_params[:range] &&
+        my_params[:range].to_unsafe_h.any? do |key, v|
+          v.present? && v.respond_to?(:'[]') &&
+          (v["begin"].present? || v["end"].present? || v["missing"].present?)
+        end
     end
   end
 end

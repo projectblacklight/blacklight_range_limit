27d4f6c92a16082067371941c3df5e782c6d5f9b
Jack Reed <phillipjreed@gmail.com>
2019-11-26 15:53:11 -0700

    Add basic test that the right things happen in the model

--- spec/features/a_javascript_spec.rb
+++ spec/features/a_javascript_spec.rb
@@ -6,11 +6,18 @@ describe 'JavaScript', js: true do
   it 'initializes canvas chart' do
     visit search_catalog_path
 
-    within('#facets .navbar') do
-      page.find('button.navbar-toggler').click
-    end
-
     click_button 'Publication Date Sort'
     expect(page).to have_css '.flot-base'
+  end
+  it 'has a View larger modal' do
+    visit search_catalog_path
+
+    click_button 'Publication Date Sort'
+    sleep(1) # resize is debounced
+    click_link 'View larger »'
+
+    within '.modal-body' do
+      expect(page).to have_css '.flot-base'
+    end
   end
 end

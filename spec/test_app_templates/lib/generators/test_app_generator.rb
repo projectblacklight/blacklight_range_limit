b2ab8bce5bf104de0d0600670bf1df094c83f0d6
Jessie Keck <jessie.keck@gmail.com>
2020-03-30 12:13:47 -0700

    Add an assets generator method that handles generating sprockets compliant files correctly

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -30,4 +30,11 @@ class TestAppGenerator < Rails::Generators::Base
       "\n    config.add_facet_field 'pub_date_si', label: 'Publication Date Sort', range: true"
     end
   end
+
+  def assets
+    return if !defined?(Sprockets::VERSION) || Sprockets::VERSION < '4'
+
+    append_to_file 'app/assets/config/manifest.js', "\n//= link application.js"
+    empty_directory 'app/assets/images'
+  end
 end

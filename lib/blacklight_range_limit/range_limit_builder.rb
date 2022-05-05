cebdc34f72cd5a741d2c83d0a2f0810ca3e1d1dd
Benjamin Armintor <armintor@gmail.com>
2022-05-05 12:14:15 -0400

    use guard clauses to simplify RangeLimitBuilder#add_range_limit_params

--- lib/blacklight_range_limit/range_limit_builder.rb
+++ lib/blacklight_range_limit/range_limit_builder.rb
@@ -13,21 +13,21 @@ module BlacklightRangeLimit
     # Method added to to fetch proper things for date ranges.
     def add_range_limit_params(solr_params)
       ranged_facet_configs = blacklight_config.facet_fields.select { |_key, config| config.range }
+      return solr_params unless ranged_facet_configs.any?
 
-      if ranged_facet_configs.any?
-        solr_params["stats"] = "true"
-        solr_params["stats.field"] ||= []
-      end
+      solr_params["stats"] = "true"
+      solr_params["stats.field"] ||= []
 
       ranged_facet_configs.each do |field_key, config|
         solr_params["stats.field"] << config.field
 
         range_config = config.range_config
+        next if range_config[:segments] == false
 
         selected_value = search_state.filter(config.key).values.first
         range = (selected_value if selected_value.is_a? Range) || range_config[:assumed_boundaries]
 
-        add_range_segments_to_solr!(solr_params, field_key, range.first, range.last) if range_config[:segments] != false && range.present?
+        add_range_segments_to_solr!(solr_params, field_key, range.first, range.last) if range.present?
       end
 
       solr_params

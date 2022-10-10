4c13258b5b16b0b2f77ec412697fad879b53c9f2
Justin Coyne <jcoyne@justincoyne.com>
2022-10-10 16:15:10 -0500

    Add missing variable declaration

--- app/assets/javascripts/blacklight_range_limit/range_limit_distro_facets.js
+++ app/assets/javascripts/blacklight_range_limit/range_limit_distro_facets.js
@@ -51,7 +51,7 @@ Blacklight.onLoad(function() {
   // be triggered. The function will be called after it stops being called for
   // N milliseconds. If `immediate` is passed, trigger the function on the
   // leading edge, instead of the trailing.
-  debounce = function(func, wait, immediate) {
+  const debounce = function(func, wait, immediate) {
     var timeout;
     return function() {
       var context = this, args = arguments;

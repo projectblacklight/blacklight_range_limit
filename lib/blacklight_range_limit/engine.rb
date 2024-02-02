0cd76d03a19786f19b03e1ab610e9db67c6c8bb3
Justin Coyne <jcoyne@justincoyne.com>
2024-02-02 09:04:15 -0600

    Update assets generator to use importmap

--- lib/blacklight_range_limit/engine.rb
+++ lib/blacklight_range_limit/engine.rb
@@ -11,5 +11,13 @@ module BlacklightRangeLimit
     config.before_configuration do
       Blacklight::Configuration::FacetField.prepend BlacklightRangeLimit::FacetFieldConfigOverride
     end
+
+    initializer 'blacklight_range_limit.assets', before: 'assets' do |app|
+      app.config.assets.precompile << 'blacklight_range_limit/blacklight_range_limit.esm.js'
+    end
+
+    initializer 'blacklight_range_limit.importmap', before: 'importmap' do |app|
+      app.config.importmap.paths << Engine.root.join('config/importmap.rb') if app.config.respond_to?(:importmap)
+    end
   end
 end
